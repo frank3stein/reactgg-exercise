bfeb17d588976b4564a73d39d5277127ae624df7
Emil <martinov.emil@gmail.com>
2023-10-10 13:14:32 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,217 +1,85 @@
 import * as React from "react";
 
+const initialState = {
+  past: [],
+  present: 0,
+  future: []
+};
 
-export default function MultistepFormReducer() {
-  // const [currentStep, setCurrentStep] = React.useState(1);
-  // const [formData, setFormData] = React.useState(initialFormData);
-  const [state, dispatch] = React.useReducer(formReducer, {
-    currentStep: 1,
-    formData: initialFormData
-  })
-  const handleNextStep = () => {
-    // setCurrentStep(currentStep + 1);
-    dispatch({
-      type: "NEXT_STEP",
-      payload: { currentStep: state.currentStep }
-    })
-  };
-
-  const handlePrevStep = () => {
-    // setCurrentStep(currentStep - 1);
-    dispatch({
-      type: "PREV_STEP",
-      payload: { currentStep: state.currentStep }
-    })
-  };
-
-  const handleChange = (e) => {
-    // setFormData({ ...formData, [e.target.name]: e.target.value });
-    dispatch({
-      type: "UPDATE_FORM_DATA",
-      payload: { [e.target.name]: e.target.value }
-    })
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    alert("Thank you for your submission");
-    // setCurrentStep(1);
-    // setFormData(initialFormData);
-    dispatch({
-      type: "RESET_FORM",
-      payload: { initialFormData }
-    })
-    dispatch({
-      type: "RESET_STEP",
-      payload: { currentStep: 1 }
-    })
-  };
-
-  if (state.currentStep === 1) {
-    return (
-      <form onSubmit={handleSubmit}>
-        <h2>Personal Information</h2>
-        <div>
-          <label>Step {state.currentStep} of 3</label>
-          <progress value={state.currentStep} max={3} />
-        </div>
-        <div>
-          <label htmlFor="name">Name</label>
-          <input
-            required
-            name="name"
-            id="name"
-            placeholder="Enter your name"
-            value={state.formData.name}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input
-            required
-            name="email"
-            id="email"
-            type="email"
-            placeholder="Enter your email"
-            value={state.formData.email}
-            onChange={handleChange}
-          />
-        </div>
-        <button type="button" className="secondary" onClick={handleNextStep}>
-          Next
-        </button>
-      </form>
-    );
-  } else if (state.currentStep === 2) {
-    return (
-      <form onSubmit={handleSubmit}>
-        <h2>Address</h2>
-        <div>
-          <label>Step {state.currentStep} of 3</label>
-          <progress value={state.currentStep} max={3} />
-        </div>
-        <div>
-          <label htmlFor="address">Address</label>
-          <input
-            required
-            name="address"
-            id="address"
-            type="address"
-            placeholder="What is your address?"
-            value={state.formData.address}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="city">City</label>
-          <input
-            required
-            name="city"
-            id="city"
-            placeholder="What city do you live in?"
-            value={state.formData.city}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="zipcode">Zipcode</label>
-          <input
-            required
-            name="zipcode"
-            id="zipcode"
-            type="number"
-            placeholder="What is your zipcode?"
-            value={state.formData.zipcode}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <button className="secondary" type="button" onClick={handleNextStep}>
-            Next
-          </button>
-          <button type="button" className="link" onClick={handlePrevStep}>
-            Previous
-          </button>
-        </div>
-      </form>
-    );
-  } else if (state.currentStep === 3) {
-    return (
-      <form onSubmit={handleSubmit}>
-        <h2>Confirm your information:</h2>
-        <div>
-          <label>Step {state.currentStep} of 3</label>
-          <progress value={state.currentStep} max={3} />
-        </div>
-        <table>
-          <tbody>
-            {Object.keys(state.formData).map((key) => {
-              return (
-                <tr key={key}>
-                  <td>{key}</td>
-                  <td>{state.formData[key]}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-        <div>
-          <button className="primary" type="submit">
-            Submit
-          </button>
-          <button type="button" className="link" onClick={handlePrevStep}>
-            Previous
-          </button>
-        </div>
-      </form>
-    );
-  } else {
-    return null;
+function reducer(state, action) {
+  const { past, present, future } = state;
+  switch(action.type) {
+  case "INCREMENT":
+    return {
+      past: [...past, present],
+      present: present + 1,
+      future: []
+    };
+  case "DECREMENT":
+    return {
+      past: [...past, present],
+      present: present - 1,
+      future: []
+    };
+  case "UNDO":
+    return {
+      // slice is used to remove the last element from the array
+      past: past.slice(0, past.length - 1),
+      present: past[past.length - 1],
+      future: [present, ...future]
+    };
+  case "REDO":
+    return {
+      past: [...past, present],
+      present: future[0],
+      // slice is used to remove the first element from the array
+      future: future.slice(1)
+    };
+    default:
+      return state;
   }
 }
 
+export default function CounterWithUndoRedo() {
+  // const state = initialState;
+  const [state, dispatch] = React.useReducer(reducer, initialState);
 
+  const handleIncrement = () => {
+    dispatch({ type: "INCREMENT" })
+  };
+  const handleDecrement = () => {
+    dispatch({ type: "DECREMENT" })
+  };
+  const handleUndo = () => {
+    dispatch({ type: "UNDO" })
+  };
+  const handleRedo = () => {
+    dispatch({ type: "REDO" })
+  };
 
-const initialFormData = {
-  name: "",
-  email: "",
-  address: "",
-  city: "",
-  zipcode: ""
-};
-
-const formReducer = (state, action) => {
-  switch(action.type) {
-    case "NEXT_STEP":
-      return {
-        ...state,
-        currentStep: action.payload.currentStep + 1
-      }
-    case "PREV_STEP":
-      return {
-        ...state,
-        currentStep: action.payload.currentStep - 1
-      }
-    case "UPDATE_FORM_DATA":
-      return {
-        ...state,
-        formData: {
-          ...state.formData,
-          ...action.payload
-        }
-      }
-    case "RESET_FORM":
-      return {
-        ...state,
-        formData: action.payload.initialFormData
-      }
-    case "RESET_STEP":
-      return {
-        ...state,
-        currentStep: action.payload.currentStep
-      }
-    default:  
-      return state;
-  }
+  return (
+    <div>
+      <h1>Counter: {state.present}</h1>
+      <button className="link" onClick={handleIncrement}>
+        Increment
+      </button>
+      <button className="link" onClick={handleDecrement}>
+        Decrement
+      </button>
+      <button
+        className="link"
+        onClick={handleUndo}
+        disabled={!state.past.length}
+      >
+        Undo
+      </button>
+      <button
+        className="link"
+        onClick={handleRedo}
+        disabled={!state.future.length}
+      >
+        Redo
+      </button>
+    </div>
+  );
 }
